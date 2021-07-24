fb2321b47aa82938db8e48ad604b4ec6a2d40265
aachalmakode <aachalmakode900@gmail.com>
2021-07-24 15:21:25 -0400

    Added : Creat Table

--- Addressboo_DB.sql
+++ Addressboo_DB.sql
@@ -1,3 +1,17 @@
 create database addressbook;
 show databases;
 use addressbook;
+
+#UC2-creates a Table
+create table addressbook (
+first_name varchar(31) not null,
+last_name varchar(31) not null,
+address varchar(150) not null,
+city varchar(31) not null,
+state varchar(31) not null,
+zip int not null,
+phone_number int not null,
+email varchar(50) not null
+);
+select * from addressbook;
+
