82478b54b3c662e5582b968f571fc3f3537a6472
aachalmakode <aachalmakode900@gmail.com>
2021-07-25 20:17:57 -0400

    Added: To get num of contact by type

--- Addressboo_DB.sql
+++ Addressboo_DB.sql
@@ -56,6 +56,7 @@ update addresbook set type='FRIEND' where first_name='Yash' or first_name='Minal
 update addressbook set type='PROFESSION' where first_name='Neha' or first_name='Vaishnavi';
 select * from addressbook;
 
+
 #UC10 To get num contacts by type
 select type, count(first_name) from addressbook group by type;
  select * from addressbook;
